c9a7d9777aa168b4aab94afe91c60afe1f38c203
namanchandak <naman.chandak@hotmail.com>
2024-01-19 18:16:56 +0530

    login and signup ui

---
+++ src/app/MyComponents/books-item/books-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './books-item.component.html',
+  styleUrl: './books-item.component.css'
+})
+export class BooksItemComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from "./MyComponents/login/login.component";
+import { SignupComponent } from "./MyComponents/signup/signup.component";
 
-export const routes: Routes = [];
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent},
+  ];
+
+export const routes: Routes = appRoutes;
