ecb6a7482a5387f3fdc3f6856f86f2e73abf173e
namanchandak <naman.chandak@hotmail.com>
2024-01-19 19:41:12 +0530

    login data reciving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./MyComponents/login/login.component";
 import { SignupComponent } from "./MyComponents/signup/signup.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, LoginComponent, SignupComponent]
+    imports: [CommonModule, RouterOutlet, LoginComponent, SignupComponent, HttpClientModule, FormsModule]
 })
 export class AppComponent {
   title = 'BookApp';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from "./MyComponents/login/login.component";
 import { SignupComponent } from "./MyComponents/signup/signup.component";
+import { BooksComponent } from './MyComponents/books/books.component';
 
 const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: '', component: BooksComponent },
     { path: 'signup', component: SignupComponent},
   ];
 
