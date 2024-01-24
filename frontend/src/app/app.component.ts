6db51980eda3a5ffb5808c5d5c156ef4f9787e78
namanchandak <naman.chandak@hotmail.com>
2024-01-25 01:54:05 +0530

    logout and navbar button fixed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./MyComponents/login/login.component";
 import { SignupComponent } from "./MyComponents/signup/signup.component";
@@ -11,8 +11,20 @@ import { FormsModule } from '@angular/forms';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, LoginComponent, SignupComponent, HttpClientModule, FormsModule]
+    imports: [CommonModule, RouterOutlet, LoginComponent, SignupComponent, HttpClientModule, FormsModule, NgIf]
 })
 export class AppComponent {
   title = 'BookApp';
+  isTokenExist(): boolean {
+    if (typeof localStorage !== 'undefined') {
+      const jwtToken = localStorage.getItem('loginToken');
+      return !!jwtToken; // Return true if the token exists, false otherwise
+    } else {
+      return false; // localStorage is not defined, handle accordingly
+    }
+  }
+  logout()
+  {
+    localStorage.removeItem('loginToken');
+  }
 }
