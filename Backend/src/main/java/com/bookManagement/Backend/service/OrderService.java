bc620a63a42bd4a6f6cf1a36015dda1f8c091566
KunalPratap28 <kunal.p@consultadd.com>
2024-01-30 23:45:10 +0530

    buy-book

--- Backend/src/main/java/com/bookManagement/Backend/service/OrderService.java
+++ Backend/src/main/java/com/bookManagement/Backend/service/OrderService.java
@@ -22,26 +22,15 @@ public class OrderService {
 
 
 
-    public List<Map<String,String>> getAllBooks(String buyerId){
+    public List<Book> getAllBooks(String buyerId){
         List<Order> orders=orderRepo.findOrderBy(buyerId);
         if(orders.isEmpty())
             return new ArrayList<>();
-        List<Map<String,String>> books=new ArrayList<>();
+        List<Book> books=new ArrayList<>();
         for(Order o:orders){
             Optional<Book> b=bookService.findBook(o.getBookId(),o.getSellerId());
-            Map<String,String> m=new HashMap<>();
             if(b.isPresent()){
-                m.put("bookId",b.get().getBookId().toString());
-                m.put("bookName",b.get().getBookName());
-                m.put("bookDescription",b.get().getBookDescription());
-                m.put("price",b.get().getPrice().toString());
-                m.put("author",b.get().getAuthor());
-                m.put("quantity",b.get().getQuantity().toString());
-                m.put("sellerId",b.get().getSellerId());
-                m.put("genre",b.get().getGenre().toString());
-                m.put("date",o.getDate().toString());
-                books.add(m);
-
+                books.add(b.get());
             }
         }
         return books;
